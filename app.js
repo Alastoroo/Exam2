d635ba0a2ce3f87888654079198153f73735b2e7
Colin <colinb.dev@gmail.com>
2016-11-09 14:16:08 +0100

    + controller directive

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 (function(){
 
+var app = angular.module("app", []);
+
+  app.controller("appController", function(){
+   this.personne = object;
+
+  });
 
 
+  app.directive("footer", function(){
+    return{
+      restrict : 'A',
+      templateUrl : "partials/common/footer.html"
+    }
+  });
+  app.directive('header', function(){
+    return{
+      restrict : 'A',
+      templateUrl : "partials/common/header.html"
+    }
+  });
+
+  app.directive('home', function(){
+      return{
+        restrict : 'A',
+        templateUrl : 'partials/home/home.html'
+      }
+  });
 
 
 
